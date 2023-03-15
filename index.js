3290028797c5c89dc561af08db672d19bdae8929
smulch <mulcahy.samuel@gmail.com>
2023-03-14 22:48:46 -0400

    trying to retrieve an id value based on user input, had to give up and create simple switch statement placeholder

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
+// using mysql2 to connect to my local sql server.
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -15,6 +16,8 @@ const testFunction = () => {
   console.log("testFunction success!");
 };
 
+// This list is the array inquirer uses to display user choices on initialization
+// the value key pair is an object because, for whatever reason, when it linked the function directly, it did not actually link the function. It works when it one layer deeper into an object so that's how it is.
 const firstChoices = [
   {
     name: "View all departments",
@@ -38,16 +41,22 @@ const firstChoices = [
   },
   {
     name: "Add a role",
-    value: { fn: testFunction },
+    value: { fn: addRole },
     short: "Add a role:",
   },
   {
     name: "Add an employee",
-    value: { fn: testFunction },
+    value: { fn: addEmployee },
     short: "Add an employee:",
+  },
+  {
+    name: "Update an employee role",
+    value: { fn: updateRole },
+    short: "Update an employee role:",
   },
 ];
 
+// this function calls on initialization and whenever a user reaches the end of an inquiry tree.
 function initialize() {
   inquirer
     .prompt([
@@ -61,6 +70,7 @@ function initialize() {
     .then((data) => data.initialize.fn());
 }
 
+// this function prints a table of departments info with the data returned from the query
 function viewDepts() {
   db.query(`SELECT * FROM departments`, (err, result) => {
     if (err) {
@@ -71,30 +81,39 @@ function viewDepts() {
   });
 }
 
+// this function prints a table of roles info with the data returned from the query
 function viewRoles() {
-  db.query(`SELECT roles.title, roles.id, roles.salary, departments.name FROM roles JOIN departments ON roles.dept_id = departments.id`, (err, result) => {
-    if (err) {
-      console.error(err);
+  db.query(
+    `SELECT roles.title, roles.id, roles.salary, departments.name FROM roles JOIN departments ON roles.dept_id = departments.id`,
+    (err, result) => {
+      if (err) {
+        console.error(err);
+      }
+      console.table(result);
+      initialize();
     }
-    console.table(result);
-    initialize();
-  });
+  );
 }
 
+// this function prints a table of employeess info with the data returned from the query
 function viewEmployees() {
-  db.query(`SELECT employee.id, employee.first_name, employee.last_name, roles.title, roles.salary, departments.name AS department, CONCAT(manager.first_name,' ',manager.last_name) AS manager 
+  db.query(
+    `SELECT employee.id, employee.first_name, employee.last_name, roles.title, roles.salary, departments.name AS department, CONCAT(manager.first_name,' ',manager.last_name) AS manager 
   FROM Employees employee
   JOIN roles ON employee.role_id = roles.id
   JOIN departments ON roles.dept_id = departments.id
-  LEFT JOIN Employees manager ON employee.manager_id = manager.id;`, (err, result) => {
-    if (err) {
-      console.error(err);
+  LEFT JOIN Employees manager ON employee.manager_id = manager.id;`,
+    (err, result) => {
+      if (err) {
+        console.error(err);
+      }
+      console.table(result);
+      initialize();
     }
-    console.table(result);
-    initialize();
-  });
+  );
 }
 
+// this function adds a department to the departments table based on what the user inputs.
 function addDept() {
   inquirer
     .prompt([
@@ -119,5 +138,96 @@ function addDept() {
     });
 }
 
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter a title for the role",
+        name: "roleName",
+      },
+      {
+        type: "number",
+        message: "Please enter a salary for the role",
+        name: "roleSalary",
+      },
+      {
+        type: "input",
+        message: "Please enter the department for the role",
+        name: "roleDept",
+      },
+    ])
+    .then((data) => {
+      var tempID = getDeptId(data.roleDept);
+      data.id = tempID;
+      return data;
+    })
+    .then((data) => {
+      db.query(
+        `INSERT INTO roles (title, salary, dept_id)
+      VALUES (${data.roleName}, ${data.roleSalary}, ${data.id})`,
+        (err, result) => {
+          if (err) {
+            console.error(err);
+          }
+          console.log(`${data.roleName} successfully added to database!`);
+        }
+      );
+      viewRoles();
+    });
+}
 
-initialize();
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter a name for the department",
+        name: "dptName",
+      },
+    ])
+    .then((data) => {
+      db.query();
+    });
+}
+
+function updateRole() {}
+
+function getDeptId(deptName) {
+  //   let data = db.query(
+  //     `SELECT id FROM departments WHERE name='${deptName}'`).then(
+  //     (err, result) => {
+  //       if (err) {
+  //         console.error(err);
+  //         return 0;
+  //       }
+  //       console.log(result);
+  //       return result[0].id;
+  //     }
+  //   )
+  //   console.log(data)
+  //   return data;
+  let id;
+  switch (deptName) {
+    case "Finance":
+      id = 1;
+      break;
+    case "Human Relations":
+      id = 2;
+      break;
+    case "Sales":
+      id = 3;
+      break;
+    case "Executive Management":
+      id = 4;
+      break;
+    default:
+      id = 0;
+  }
+  console.log(id);
+  return id;
+}
+
+getDeptId("Finance");
+
+// initialize();
